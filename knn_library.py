69101b9f54ad308d6d6b1cb321fbe470c034a89e
noah-daniel-mancino <noahdanielmancino@gmail.com>
2020-01-11 15:00:22 -0800

    Update knn_library.py

--- knn_library.py
+++ knn_library.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def ordered_distances(target_vector:list, crowd_table, answer_column:str, dfunc:Callable) -> list:
+def ordered_distances(target_vector:list, crowd_table, answer_column:str, dfunc) -> list:
   assert isinstance(target_vector, list), f'target_vector not a list but instead a {type(target_vector)}'
   assert isinstance(crowd_table, pd.core.frame.DataFrame), f'crowd_table not a dataframe but instead a {type(crowd_table)}'
   assert isinstance(answer_column, str), f'answer_column not a string but instead a {type(answer_column)}'
@@ -16,7 +16,7 @@ def ordered_distances(target_vector:list, crowd_table, answer_column:str, dfunc:
   distance_list.sort(key = lambda dist_tup: dist_tup[1])
   return distance_list
 
-def knn(target_vector:list, crowd_table, answer_column:str, k:int, dfunc:Callable) -> int:
+def knn(target_vector:list, crowd_table, answer_column:str, k:int, dfunc) -> int:
   assert isinstance(target_vector, list), f'target_vector not a list but instead a {type(target_vector)}'
   assert isinstance(crowd_table, pd.core.frame.DataFrame), f'crowd_table not a dataframe but instead a {type(crowd_table)}'
   assert isinstance(answer_column, str), f'answer_column not a string but instead a {type(answer_column)}'
@@ -28,7 +28,7 @@ def knn(target_vector:list, crowd_table, answer_column:str, k:int, dfunc:Callabl
   polled_survival = [crowd_table.loc[i, 'Survived'] for i in polled_id]
   return int(sum(polled_survival)/len(polled_survival) > .5)
   
-def knn_accuracy_tester(test_table:dframe, crowd_table, answer_column:str, k:int, dfunc:Callable) -> float:
+def knn_accuracy_tester(test_table:dframe, crowd_table, answer_column:str, k:int, dfunc) -> float:
   assert isinstance(test_table, pd.core.frame.DataFrame), f'test_table not a dataframe but instead a {type(test_table)}'
   assert isinstance(crowd_table, pd.core.frame.DataFrame), f'crowd_table not a dataframe but instead a {type(crowd_table)}'
   assert isinstance(answer_column, str), f'answer_column not a string but instead a {type(answer_column)}'
@@ -47,7 +47,7 @@ def knn_accuracy_tester(test_table:dframe, crowd_table, answer_column:str, k:int
 
   return points/possible_points
   
-def knn_confusion(test_table, crowd_table, answer_column, k, dfunc:Callable) -> dict:
+def knn_confusion(test_table, crowd_table, answer_column, k, dfunc) -> dict:
   assert isinstance(test_table, pd.core.frame.DataFrame), f'test_table not a dataframe but instead a {type(test_table)}'
   assert isinstance(crowd_table, pd.core.frame.DataFrame), f'crowd_table not a dataframe but instead a {type(crowd_table)}'
   assert isinstance(answer_column, str), f'answer_column not a string but instead a {type(answer_column)}'
